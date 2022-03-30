0fac177e827619554ff5f7ca0fb3e1301b01ab65
[Alexandr] <[afominyh242@gmail.com]>
2022-03-31 01:25:24 +0700

    final

--- Module1/weather_in_sanfrancisco.py
+++ Module1/weather_in_sanfrancisco.py
@@ -2,11 +2,12 @@
 import requests
 
 url_template = "https://wttr.in/{}"
+
 cities = ["svo", "London", "Череповец"]
 parameters = {'lang': 'ru'}
+
 for city in cities:
-    # print(city.)
     url = url_template.format(str(city))
-    print(url)
     html_string = requests.get(url, params=parameters)
+    html_string.raise_for_status()
     print(html_string.text)
