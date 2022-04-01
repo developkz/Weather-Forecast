0978f9e21d95a80fcb487d1629ccb4b87fd9ec0f
[Alexandr] <[afominyh242@gmail.com]>
2022-04-01 10:31:21 +0700

    for better reading code

--- weather.py
+++ weather.py
@@ -6,6 +6,7 @@ url_template = "https://wttr.in/{}"
 cities = ["svo", "London", "Череповец"]
 parameters = {'lang': 'ru'}
 
+
 for city in cities:
     url = url_template.format(str(city))
     string_data = requests.get(url, params=parameters)
