2b9486ea611291d53d9a0cbef1e92b59bd3085e4
[Alexandr] <[afominyh242@gmail.com]>
2022-04-01 22:00:41 +0700

    code reafctoring because last teacher comments

--- weather.py
+++ weather.py
@@ -1,14 +1,19 @@
-
 import requests
 
-url_template = "https://wttr.in/{}"
 
-cities = ["svo", "London", "Череповец"]
+locations = [
+    'svo', 
+    'London', 
+    'Череповец'
+]
+
 parameters = {'lang': 'ru'}
 
 
-for city in cities:
-    url = url_template.format(str(city))
-    string_data = requests.get(url, params=parameters)
-    string_data.raise_for_status()
-    print(string_data.text)
+for location in locations:
+    url_template = 'https://wttr.in/{0}{1}'
+    url = url_template.format(location, '?Tn')
+    # print(url)
+    weather_request = requests.get(url, params=parameters)
+    weather_request.raise_for_status()
+    print(weather_request.text)
